944865714c9411b9f9d11f38eecd5deb8a5483d1
Eduardo Coelho <eceduardocoelho@gmail.com>
2023-05-30 15:07:52 -0300

    Fixed the simplification problem

--- CalculoIntegrais.py
+++ CalculoIntegrais.py
@@ -17,7 +17,7 @@ def beggining_scream():
                           font='arial 8',
                           pad=(3, 0),
                           key='-LOWER-'),
-                 sg.Input(size=(20, 0),
+                 sg.Input(size=(40, 0),
                           font='arial 15',
                           pad=(0, 0),
                           key='-FUNCTION-')
@@ -30,11 +30,11 @@ def beggining_scream():
              [sg.Text('Resolução para chegar no passo n4: ', font='arial 12')],
              [sg.Text('Resultado da integral: ', font='arial 15')]]
 
-  column2 = [[sg.Text(font='arial 13 bold', key='-STEP1-', size=(20, 1))],
-             [sg.Text(font='arial 13 bold', key='-STEP2-', size=(20, 1))],
-             [sg.Text(font='arial 13 bold', key='-STEP3-', size=(20, 1))],
-             [sg.Text(font='arial 13 bold', key='-STEP4-', size=(20, 1))],
-             [sg.Text(font='arial 15 bold', key='-OUT-', size=(20, 1))]]
+  column2 = [[sg.Text(font='arial 13 bold', key='-STEP1-', size=(30, 1))],
+             [sg.Text(font='arial 13 bold', key='-STEP2-', size=(30, 1))],
+             [sg.Text(font='arial 13 bold', key='-STEP3-', size=(30, 1))],
+             [sg.Text(font='arial 13 bold', key='-STEP4-', size=(30, 1))],
+             [sg.Text(font='arial 15 bold', key='-OUT-', size=(30, 1))]]
 
   buttons = [[
     sg.Button('Calcular',
@@ -58,7 +58,7 @@ def beggining_scream():
   window = sg.Window('CalculadoraDeIntegrais',
                      element_padding=(0, 10),
                      layout=layout,
-                     size=(600, 500),
+                     size=(700, 500),
                      finalize=True)
 
 
@@ -70,10 +70,11 @@ while True:
   if events == "-CALCULATE-":
     x = Symbol('x')
     expr = apart(values['-FUNCTION-'])
-    upper = apart(values['-UPPER-'])
-    lower = apart(values['-LOWER-'])
-    result = str(integrate(expr, (x, lower, upper))).replace("log",
-                                                     "ln").replace("**", "ˆ")
+    upper = values['-UPPER-']
+    lower = values['-LOWER-']
+    result = integrate(expr, (x, lower, upper))
+    result = factor(result)
+    result = str(logcombine(result.expand(), force=true)).replace("log", "ln").replace("**", "^")
     window['-OUT-'].update(result)
 
   if events == sg.WIN_CLOSED:
